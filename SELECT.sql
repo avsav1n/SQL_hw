e33bc6db37cabc65e2aad1216cc2f7cc57646be8
Aleksei Savin <166250113+avsav1n@users.noreply.github.com>
2024-06-29 20:47:16 +0530

    Bug fixed

--- SELECT.sql
+++ SELECT.sql
@@ -5,7 +5,7 @@
 --Название и продолжительность самого длительного трека
 SELECT track_name, track_duration
 FROM tracks
-WHERE track_duration = (SELECT max(track_duration) FROM tracks);
+WHERE track_duration = (SELECT MAX(track_duration) FROM tracks);
 
 --Название треков, продолжительность которых не менее 3,5 минут
 SELECT track_name
@@ -32,7 +32,7 @@ WHERE track_name LIKE '%My%';
 */
 
 --Количество исполнителей в каждом жанре
-SELECT genre_name, count(a.artist_id)
+SELECT genre_name, COUNT(a.artist_id)
 FROM genres_artists ga
 JOIN genres g ON g.genre_id = ga.genre_id 
 JOIN artists a ON ga.artist_id = a.artist_id
@@ -41,11 +41,11 @@ GROUP BY g.genre_name;
 --Количество треков, вошедших в альбомы 2019–2020 годов
 SELECT COUNT(track_name)
 FROM albums a
-LEFT JOIN tracks t ON a.album_id = t .album_id
+LEFT JOIN tracks t ON a.album_id = t.album_id
 WHERE album_year BETWEEN 2019 AND 2020;
 
 --Средняя продолжительность треков по каждому альбому
-SELECT album_name, avg(track_duration) 
+SELECT album_name, AVG(track_duration) 
 FROM albums a
 LEFT JOIN tracks t ON a.album_id = t.album_id
 GROUP BY album_name;
@@ -84,7 +84,7 @@ WHERE ar.artist_name IN (
 						 JOIN artists ar ON ga.artist_id = ar.artist_id 
 						 JOIN genres g ON ga.genre_id = g.genre_id 
 						 GROUP BY ar.artist_name
-						 HAVING count(g.genre_name) > 2
+						 HAVING COUNT(g.genre_name) > 2
 						)
 GROUP BY al.album_name;
 
@@ -104,7 +104,7 @@ JOIN artists ar ON aa.artist_id = ar.artist_id
 JOIN albums al ON aa.album_id  = al.album_id 
 JOIN tracks t ON al.album_id = t.album_id
 WHERE t.track_duration = (
-						   SELECT min(track_duration)
+						   SELECT MIN(track_duration)
 						   FROM tracks t
 						  )
 GROUP BY ar.artist_name;
@@ -114,10 +114,10 @@ SELECT album_name
 FROM tracks t
 JOIN albums a ON a.album_id = t.album_id 
 GROUP BY a.album_name
-HAVING count(t.track_id) = (
-							SELECT min(quantity)
+HAVING COUNT(t.track_id) = (
+							SELECT MIN(quantity)
 							FROM (
-								  SELECT count(t.track_id) quantity
+								  SELECT COUNT(t.track_id) quantity
 								  FROM tracks t
 								  GROUP BY t.album_id
 								 )
